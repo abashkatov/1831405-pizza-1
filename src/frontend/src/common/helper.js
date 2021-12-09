7e0644e3133f4dd44612b1063e816472ab7ed837
Aleksey Bashkatov <a.bashkatov@corp.vk.com>
2021-12-10 00:48:46 +0300

    adds api and jwt services as plugins

--- src/frontend/src/common/helper.js
+++ src/frontend/src/common/helper.js
@@ -1,2 +1,11 @@
+import resources from "@/common/enums/resources";
+import { AuthApiService } from "@/services/api.service";
+
 export const capitalize = (string) =>
   `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+
+export const createResources = (notifier) => {
+  return {
+    [resources.AUTH]: new AuthApiService(notifier),
+  };
+};
--- src/frontend/src/plugins/vuePlugins.js
+++ src/frontend/src/plugins/vuePlugins.js
@@ -1,12 +1,18 @@
 import Vue from "vue";
+import JWTService from "@/services/jwt.service";
 import Notifier from "@/plugins/notifier";
 import store from "@/store";
+import { createResources } from "@/common/helper";
 
 const plugins = {
   install(Vue) {
     Vue.mixin({
       computed: {
+        $jwt: () => JWTService,
         $notifier: () => new Notifier(store),
+        $api() {
+          return createResources(this.$notifier);
+        },
       },
     });
   },
